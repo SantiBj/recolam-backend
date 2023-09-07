c2d972a4b1d266ec9671837561da8a1da0a45f0d
SantiBj <lsantibz@gmail.com>
2023-09-06 19:28:14 -0500

    añadiendo el campo address

--- trips/views/TripViews.py
+++ trips/views/TripViews.py
@@ -15,7 +15,7 @@ class TripsAvailableForDate(RetrieveAPIView):
                 kwargs["date"], '%Y-%m-%d').date()
             number_trips_for_day = Trip.objects.filter(
                 Q(scheduleDay=date) & Q(isDisable=False)).count()
-            available = True if number_trips_for_day < 20 else False
+            available = True if number_trips_for_day < 1 else False
             return Response({"avaliable": bool(available)}, status=status.HTTP_200_OK)
         except ValueError as e:
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
